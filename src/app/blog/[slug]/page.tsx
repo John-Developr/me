386118281c23b9b01777f8015a03d825917d384d
John <johncarlo.fullstackdev@gmail.com>
2026-02-25 17:15:43 +0800

    Added fixed
    - Deployment Issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import BlogDetailPage from "@/containers/blog-detail-page";
 
-export default function BlogDetail({ params }: { params: { slug: string } }) {
+// 1. Define the type where params is a Promise
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+// 2. Make the component async and await the params
+export default async function BlogDetail({ params }: Props) {
+  const { slug } = await params;
+
   return (
-    <BlogDetailPage slug={params.slug} />
-  )
+    <BlogDetailPage slug={slug} />
+  );
 }
