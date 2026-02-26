ea904bcf693c98f023a531ad6d69a926658d15e2
John <johncarlo.fullstackdev@gmail.com>
2026-02-26 16:30:57 +0800

    Added fixed.
    - Removed unnecessary changes

--- src/containers/home-page/connect-section/ConnectSection.tsx
+++ src/containers/home-page/connect-section/ConnectSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -7,10 +7,6 @@ import { HomeIcons } from "@/components/icons";
 import styles from "@/styles/pages/page.module.css";
 import { networkDefine } from "@/config/networkDefine";
 import CopyClipboard from "@/components/clipoard";
-
-interface SVGPropsWithVisibility extends React.SVGProps<SVGSVGElement> {
-      isVisible?: boolean;
-}
 
 export default function ConnectionSection() {
     return (
@@ -93,41 +89,4 @@ export default function ConnectionSection() {
             </section>
         </section>  
     )
-}
-
-function Clippy(props: SVGPropsWithVisibility) {
-  return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M5.75 4.75H10.25V1.75H5.75V4.75Z" />
-      <path d="M3.25 2.88379C2.9511 3.05669 2.75 3.37987 2.75 3.75001V13.25C2.75 13.8023 3.19772 14.25 3.75 14.25H12.25C12.8023 14.25 13.25 13.8023 13.25 13.25V3.75001C13.25 3.37987 13.0489 3.05669 12.75 2.88379" />
-    </svg>
-  );
-}
-
-function Check({ isVisible = true, ...props }: SVGPropsWithVisibility) {
-  return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M13.25 4.75L6 12L2.75 8.75" />
-    </svg>
-  );
 }
